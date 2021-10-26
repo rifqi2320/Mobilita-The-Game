005905f7f7de0372e8f30c1d9d833ce4be6650e1
Zeus-s <kristabdixx@gmail.com>
2021-10-26 12:58:14 +0700

    modified tokenmachine for io.h

---
+++ tests/driver_token.c
@@ -0,0 +1,27 @@
+#include "../src/tokenmachine.h"
+#include <stdio.h>
+
+int main() {
+	startToken();
+	if (endToken) {
+        printf("Ekspresi kosong\n");
+        return 0;
+	}
+    
+    if (currentToken.tkn == 'b') {
+        printf("%d ", currentToken.val);
+    } else {
+        printf("%c ", currentToken.tkn);
+    }
+
+    while (!endToken) {
+        if (currentToken.tkn == 'b') {
+            printf("%d ", currentToken.val);
+        } else {
+            printf("%c ", currentToken.tkn);
+        }
+        advToken();
+    }
+
+	return 0;
+}
