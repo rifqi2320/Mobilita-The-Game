6d3d36edfd7183d011a0024ba3f343ce77bcdb44
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2021-10-27 17:20:01 +0700

    Inisialisasi engine.h,engine.c, serta menambahkan variabel eksternal

---
+++ src/engine.c
@@ -0,0 +1,1 @@
+/* Implementasi Untuk Game Engine*/
---
+++ src/engine.h
@@ -0,0 +1,8 @@
+/* Game Engine untuk menhitung waktu dan skor*/
+#ifndef _ENGINE__H
+#define _ENGINE__h
+/* Variabel Global*/
+extern int waktu;//waktu yang dihabiskan Mobita
+extern int skor;//skor Mobita
+
+#endif
