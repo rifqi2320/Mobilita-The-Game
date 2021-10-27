e65221af41c33ed62c34dd356f9d0dbfbd223568
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2021-10-27 17:28:45 +0700

    Menambahkan fungsi untuk menambahkan skor

--- src/engine.c
+++ src/engine.c
@@ -1,1 +1,16 @@
 /* Implementasi Untuk Game Engine*/
+#include "engine.h"
+
+/* Variabel eksternal*/
+int waktu;//waktu yang dihabiskan Mobita
+int skor;//skor Mobita
+
+
+/* Realisasi fungsi dan prosedur*/
+void addScore(int val){
+    /*I.S. val terdefinisi*/
+    /*F.S. nilai skor bertambah sebesar val*/
+    //kamus lokal
+    //algoritma
+    skor += val;
+}
--- src/engine.h
+++ src/engine.h
@@ -1,8 +1,14 @@
 /* Game Engine untuk menhitung waktu dan skor*/
 #ifndef _ENGINE__H
 #define _ENGINE__h
+
 /* Variabel Global*/
 extern int waktu;//waktu yang dihabiskan Mobita
 extern int skor;//skor Mobita
+
+/* Fungsi dan Prosedur*/
+void addScore(int val);
+/*I.S. val terdefinisi*/
+/*F.S. nilai skor bertambah sebesar val*/
 
 #endif
