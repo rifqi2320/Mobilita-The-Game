2594c932f621f8854cb1fc699e20d095ca730b44
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2021-11-10 15:44:22 +0700

    Merapihkan ADT Item dan TodoList

--- src/todolist.h
+++ src/todolist.h
@@ -7,7 +7,6 @@ typedef List Todolist;
 
 typedef List InprogressList;
 
-typedef Item ElType;
 #define PICKUP(I) I.lPickup
 #define DROPOFF(I) I.lDropoff
 #define TYPE(I) I.type
@@ -15,9 +14,9 @@ typedef Item ElType;
 
 void CreateTodoList(Todolist *l);
 
-void addTodoList(Todolist *l, ElType task);
+void addTodoList(Todolist *l, Item task);
 
-void deleteTodoListAt(Todolist *l, ElType *task);
+void deleteTodoListAt(Todolist *l, Item *task);
 
 void displayTodoList(Todolist l);
 
@@ -25,9 +24,9 @@ boolean isVIPinList(Todolist l);
 
 void CreateInProgress(InprogressList *l);
 
-void addInProgress(InprogressList *l, ElType task);
+void addInProgress(InprogressList *l, Item task);
 
-void deleteInProgressAt(InprogressList *l, ElType *task);
+void deleteInProgressAt(InprogressList *l, Item *task);
 
 void displayInProgress(InprogressList l);
 
