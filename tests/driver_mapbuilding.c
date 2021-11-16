45d531c865e4fd4dfa088c44dc4b232a72d92b6e
Zeus-s <kristabdixx@gmail.com>
2021-11-16 14:06:57 +0700

    added driver mapbuilding

--- tests/driver_mapbuilding.c
+++ tests/driver_mapbuilding.c
@@ -1,6 +1,17 @@
 #include "../src/mapbuilding.h"
+#include "../src/constant.h"
 #include <stdio.h>
 
 int main() {
-    
+    MB mb;
+    CreateMB(&mb);
+    char building = 'C';
+    setVal(&mb, building, 1);
+    int val = getVal(mb, building);
+    printf("val building %c adalah %d\n", building, val);
+
+    building = 'B';
+    setVal(&mb, building, 0);
+    val = getVal(mb, building);
+    printf("val building %c adalah %d\n", building, val);
 }
