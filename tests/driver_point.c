220d16476b69d8f72bb6382067618c4c69514d42
Zeus-s <kristabdixx@gmail.com>
2021-11-17 21:53:39 +0700

    add driver

---
+++ tests/driver_point.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../src/point.h"
+
+int main() {
+    POINT P1, P2;
+    int x, y;
+    scanf("%d %d", &x, &y);
+    P1 = MakePOINT(x, y);
+    scanf("%d %d", &x, &y);
+    P2 = MakePOINT(x, y);
+
+    printf("Nilai kebenaran kedua point sama = %d\n", EQ(P1,P2));
+    printf("Nilai kebenaran kedua point tidak sama = %d\n", NEQ(P1,P2));
+}
