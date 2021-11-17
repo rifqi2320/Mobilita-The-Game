fd8d85d24d903097a617a68f019d7f9a271a1daa
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2021-11-17 22:09:27 +0700

    Update driver_listbuilding.c

--- tests/driver_listbuilding.c
+++ tests/driver_listbuilding.c
@@ -14,18 +14,18 @@ int main() {
 
   // Test panjang LB setelah di append
   len = lengthListBuilding(LB);
-  printf("panjang List Building = %d\n", len1);
+  printf("panjang List Building = %d\n", len);
 
   // Test panjang LB setelah dishrink
   shrinkListBuilding(&LB, 1);
   len = lengthListBuilding(LB);
-  printf("panjang List Building setelah shrink = %d\n", len1);
+  printf("panjang List Building setelah shrink = %d\n", len);
   displayListBuilding(LB);
 
   // Test panjang LB setelah dishrink lalu digrow lagi
   growListBuilding(&LB, 1);
   len = lengthListBuilding(LB);
-  printf("panjang List Building setelah di grow lagi = %d\n", len1);
+  printf("panjang List Building setelah di grow lagi = %d\n", len);
   CreateBuilding(&B, n, m, name);
   appendBuilding(&LB, B);
   displayListBuilding(LB);
